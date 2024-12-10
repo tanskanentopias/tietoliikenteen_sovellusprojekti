b54cf025e42c426fd8be53aa3ee2f715cf7d7741
JimiR0 <156087972+JimiR0@users.noreply.github.com>
2024-12-10 13:15:06 +0200

    Delete Totaalisesti_meijän_oma_koodi directory

--- Totaalisesti_meijän_oma_koodi/src/Keskipisteet.h
+++
@@ -1,7 +0,0 @@
-int final_centroids[6][3];
-final_centroids[0] = {1797, 1503, 1495};
-final_centroids[1] = {1203, 1497, 1502};
-final_centroids[2] = {1502, 1800, 1501};
-final_centroids[3] = {1496, 1199, 1496};
-final_centroids[4] = {1493, 1498, 1800};
-final_centroids[5] = {1505, 1502, 1201};
